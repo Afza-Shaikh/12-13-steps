c2e2b5341a182161bf29e6a8b9ba1fff77f61d2d
afza shaikh <shaikhafza961@gmail.com>
2024-09-16 22:01:44 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+"use strict";
+let members = ['afza', 'muniba', 'sidra', 'ayesha'];
+let message = 'hello tum kesi ho: ';
+for (let i = 0; i < members.length; i++) {
+    console.log(message + members[i]);
+}
---
+++ app.ts
@@ -0,0 +1,5 @@
+let members: string[] = ['afza','muniba','sidra','ayesha'];
+let message: string ='hello tum kesi ho: ';
+for(let i=0; i < members.length; i ++ ){
+    console.log(message + members[i]);
+}
